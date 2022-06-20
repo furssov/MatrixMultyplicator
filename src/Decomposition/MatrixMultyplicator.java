0183e8889eddf92fc96f5025960f08fd30f7a942
furssov <fursovd70@gmail.com>
2022-06-20 17:58:21 +0300

    user can multiply

--- src/Decomposition/MatrixMultyplicator.java
+++ src/Decomposition/MatrixMultyplicator.java
@@ -1,4 +1,27 @@
 package Decomposition;
 
 public class MatrixMultyplicator {
+    public Matrix multiply(Matrix p, Matrix q) throws MatrixException {
+        int v = p.getVerticalLength();
+        int h = q.getHorizontalLength();
+        int controlSize = p.getHorizontalLength();
+        if (controlSize != q.getVerticalLength()) {
+            throw new MatrixException("incompatible matrices");
+        }
+        Matrix result = new Matrix(v, h);
+        try {
+            for (int i = 0; i < v; i++) {
+                for (int j = 0; j < h; j++) {
+                    int value = 0;
+                    for (int k = 0; k < controlSize; k++) {
+                        value += p.getElement(i, k) * q.getElement(k, j);
+                    }
+                    result.setElement(i, j, value);
+                }
+            }
+        } catch (MatrixException e) {
+          // log: exception impossible
+        }
+        return result;
+    }
 }
