34e4129938963cf0f9e25132bd2922b65c73516d
furssov <fursovd70@gmail.com>
2022-06-20 17:54:01 +0300

    matrix can be filled by elemnts

--- src/Decomposition/MatrixCreator.java
+++ src/Decomposition/MatrixCreator.java
@@ -1,4 +1,16 @@
 package Decomposition;
 
 public class MatrixCreator {
+    public static void fillWithRandom(Matrix matrix, int minValue, int maxValue){
+        for (int i = 0; i < matrix.getVerticalLength(); i++){
+            for (int j = 0; j < matrix.getHorizontalLength();j++){
+                int value = (int) ((Math.random() * (maxValue - minValue)) + minValue);
+                try {
+                    matrix.setElement(i, j, value);
+                } catch (MatrixException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 }
